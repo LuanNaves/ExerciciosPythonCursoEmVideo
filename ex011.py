9460c71c48cd2f6f2ecf7951e52a95457b61291d
MurasakiNori_ <106930442+LuanNaves@users.noreply.github.com>
2022-12-26 10:01:29 -0300

    Add files via upload

---
+++ ex011.py
@@ -0,0 +1,4 @@
+largura = float(input('Largura (em metros) da parede? '))
+altura = float(input('Altura (em metros) da parede? '))
+area = largura * altura
+print(f'A área da parede é {area} metros quadrados. Será necessário {area / 2} litros de tinta para pinta-la.')
