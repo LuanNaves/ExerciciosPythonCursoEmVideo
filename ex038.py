71a69a63cc6a63fca775a243a154892e9a45d6bd
MurasakiNori_ <106930442+LuanNaves@users.noreply.github.com>
2023-01-02 11:20:46 -0300

    Add files via upload

---
+++ ex038.py
@@ -0,0 +1,8 @@
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+if num1 > num2:
+    print(f"{num1} é maior que {num2}.")
+elif num1 < num2:
+    print(f"{num1} é menor que {num2}.")
+else:
+    print(f"Os dois números são iguais.")
